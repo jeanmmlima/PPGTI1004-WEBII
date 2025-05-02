f48fe23ded3356a691f13a865ec338d036eb1bee
jeanmmlima <jeanmariomlima@gmail.com>
2025-05-02 18:04:20 -0300

    exemplo com multiplos servicos

--- inversaodecontrole/src/main/java/com/jeanlima/inversaodecontrole/controller/NotificationController.java
+++ inversaodecontrole/src/main/java/com/jeanlima/inversaodecontrole/controller/NotificationController.java
@@ -1,6 +1,7 @@
 package com.jeanlima.inversaodecontrole.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.jeanlima.inversaodecontrole.service.MensagemService;
 public class NotificationController {
 
     @Autowired
+    @Qualifier("SMSService")
     private MensagemService mensagemService;
 
     @GetMapping("/notificar")
--- inversaodecontrole/src/main/java/com/jeanlima/inversaodecontrole/service/PushNotificationService.java
+++ inversaodecontrole/src/main/java/com/jeanlima/inversaodecontrole/service/PushNotificationService.java
@@ -3,11 +3,11 @@ package com.jeanlima.inversaodecontrole.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SMSService implements MensagemService{
+public class PushNotificationService implements MensagemService{
 
     @Override
     public String enviar(String destino, String mensagem) {
-        return "Notificação enviada via SMS para " + destino + ": " + mensagem;
+        return "Notificação enviada via Push Notification para " + destino + ": " + mensagem;
     }
     
 }
