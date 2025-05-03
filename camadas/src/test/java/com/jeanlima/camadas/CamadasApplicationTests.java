8fb3fc588b87f45d2074b2e738fb6ffbb234d2e5
jeanmmlima <jeanmariomlima@gmail.com>
2025-05-03 12:27:51 -0300

    todo o codigo de camadas ok

---
+++ camadas/src/test/java/com/jeanlima/camadas/CamadasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanlima.camadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamadasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
